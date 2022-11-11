110ca68b8411357868c3886031f10f7541f9b38e
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 15:25:25 -0500

    Update Uninstall-PackageWithMsiGuid.ps1

--- Uninstall-PackageWithMsiGuid.ps1
+++ Uninstall-PackageWithMsiGuid.ps1
@@ -1,3 +1,15 @@
 $PackageName = "Dell Command | Update for Windows Universal"
-$PackageGuid = (Get-Package $PackageName).fastpackagereference
-Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart"
+
+Try {
+    Write-Host "Checking for package."
+    $PackageGuid = (Get-Package $PackageName -ErrorAction Stop).fastpackagereference
+    Write-Host "Package found.`nStarting msiexec."
+    Try {
+        Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart" -Wait -ErrorAction Stop
+        Write-Host "Uninstall complete."
+    }
+    Catch {Write-Host "Uninstall failed.`nCheck that the package has a valid MSI GUID and that the uninstall arguments are correct"}
+}
+Catch {
+    Write-Host "An error has occured.`nExiting script."
+}
