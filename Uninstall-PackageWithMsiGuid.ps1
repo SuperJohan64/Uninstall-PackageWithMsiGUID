848bece30fe2347362200c8b6e20037f01caf6cb
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 17:24:41 -0500

    Update Uninstall-PackageWithMsiGuid.ps1

--- Uninstall-PackageWithMsiGuid.ps1
+++ Uninstall-PackageWithMsiGuid.ps1
@@ -1,15 +1,19 @@
-$PackageNames = "Dell Command*Update*Windows 10*","Dell Command*Update for Windows Universal"
+$PackageNames = "Dell Command*Update*Windows 10*","Dell Command*Update for Windows Universal","Lenovo System Update"
 
 foreach ($PackageName in $PackageNames) {
     Try {
         Write-Host "`nChecking for: $PackageName"
-        $PackageGuid = (Get-Package $PackageName -ErrorAction Stop).fastpackagereference
-        Write-Host "Package found.`nStarting msiexec."
-        Try {
-            Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart" -Wait -ErrorAction Stop
-            Write-Host "Uninstall complete."
+        $PackageProviderName = ((Get-Package $PackageName -ErrorAction Stop).ProviderName)
+        if ($PackageProviderName -eq "msi") {
+            $PackageGuid = (Get-Package $PackageName -ErrorAction Stop).fastpackagereference
+            Write-Host "Package found.`nStarting msiexec."
+            Try {
+                Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart" -Wait -ErrorAction Stop
+                Write-Host "Uninstall complete."
+            }
+            Catch {Write-Host "Msiexec failed.`nCheck that the package has a valid MSI GUID and that the uninstall arguments are correct."}
         }
-        Catch {Write-Host "Msiexec failed.`nCheck that the package has a valid MSI GUID and that the uninstall arguments are correct."}
+        Else {Write-Host "Package is not a MSI.`nCould not uninstall package."}
     }
     Catch {
         Write-Host "Could not uninstall package."
