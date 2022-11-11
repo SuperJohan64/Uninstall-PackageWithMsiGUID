80d9530fda347f924d02ea3a2d2ec2765f286cfc
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 17:33:51 -0500

    Update Uninstall-PackageWithMsiGuid.ps1

--- Uninstall-PackageWithMsiGuid.ps1
+++ Uninstall-PackageWithMsiGuid.ps1
@@ -3,7 +3,7 @@ $PackageNames = "Dell Command*Update*Windows 10*","Dell Command*Update for Windo
 foreach ($PackageName in $PackageNames) {
     Try {
         Write-Host "`nChecking for: $PackageName"
-        $PackageProviderName = ((Get-Package $PackageName -ErrorAction Stop).ProviderName)
+        $PackageProviderName = (Get-Package $PackageName -ErrorAction Stop).ProviderName
         if ($PackageProviderName -eq "msi") {
             $PackageGuid = (Get-Package $PackageName -ErrorAction Stop).fastpackagereference
             Write-Host "Package found.`nStarting msiexec."
