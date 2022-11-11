fbad57a16970556a145612fa8b9b704477ad407b
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 11:08:51 -0500

    Create Get-PackageInfo.ps1

---
+++ Get-PackageInfo.ps1
@@ -0,0 +1,18 @@
+$Packages = Get-Package
+$PackageInfo = @()
+
+
+foreach ($Package in $Packages) {
+    $PackageInfo += New-Object -TypeName psobject -Property @{
+        Name = $Package.Name
+        Version = $Package.Version
+        Source = $Package.Source
+        ProviderName = $Package.ProviderName
+        MsiGuid = $Package.fastpackagereference
+    }
+}
+
+$PackageInfo `
+    | Select-Object Name, Version, Source, ProviderName, MsiGuid `
+    | Sort-Object Name `
+    | Out-GridView
