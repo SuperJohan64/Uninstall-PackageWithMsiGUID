6a2e7cea91126aec66b985d4e07fa1f02940eba7
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 11:16:02 -0500

    Update Uninstall-PackageWithMsiGuid.ps1

--- Uninstall-PackageWithMsiGuid.ps1
+++ Uninstall-PackageWithMsiGuid.ps1
@@ -1,3 +1,3 @@
 $PackageName = "Dell Command | Update for Windows Universal"
-$PackageGUID = (Get-Package $PackageName).fastpackagereference
-Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGUID /qn /norestart"
+$PackageGuid = (Get-Package $PackageName).fastpackagereference
+Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart"
