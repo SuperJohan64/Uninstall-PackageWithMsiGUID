c4ef71920c1b32c3ddd6632c352f130c9ffa1c59
SuperJohan64 <60057644+SuperJohan64@users.noreply.github.com>
2022-11-11 16:18:26 -0500

    Update Uninstall-PackageWithMsiGuid.ps1

--- Uninstall-PackageWithMsiGuid.ps1
+++ Uninstall-PackageWithMsiGuid.ps1
@@ -1,15 +1,17 @@
-$PackageName = "Dell Command | Update for Windows Universal"
+$PackageNames = "Dell Command*Update*Windows 10*","Dell Command*Update for Windows Universal"
 
-Try {
-    Write-Host "Checking for package."
-    $PackageGuid = (Get-Package $PackageName -ErrorAction Stop).fastpackagereference
-    Write-Host "Package found.`nStarting msiexec."
+foreach ($PackageName in $PackageNames) {
     Try {
-        Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart" -Wait -ErrorAction Stop
-        Write-Host "Uninstall complete."
+        Write-Host "`nChecking for: $PackageName"
+        $PackageGuid = (Get-Package $PackageName -ErrorAction Stop ).fastpackagereference
+        Write-Host "Package found.`nStarting msiexec."
+        Try {
+            Start-Process -FilePath msiexec.exe -ArgumentList "/x $PackageGuid /qn /norestart" -Wait -ErrorAction Stop
+            Write-Host "Uninstall complete."
+        }
+        Catch {Write-Host "Uninstall failed.`nCheck that the package has a valid MSI GUID and that the uninstall arguments are correct"}
     }
-    Catch {Write-Host "Uninstall failed.`nCheck that the package has a valid MSI GUID and that the uninstall arguments are correct"}
-}
-Catch {
-    Write-Host "An error has occured.`nExiting script."
+    Catch {
+        Write-Host "Could not uninstall package."
+    }
 }
